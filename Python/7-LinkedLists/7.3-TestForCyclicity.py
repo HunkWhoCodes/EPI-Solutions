b1855543545376d8933f20df3e64a5fb882baaa8
HunkWhoCodes <85839035+HunkWhoCodes@users.noreply.github.com>
2022-07-10 19:02:07 +0530

    Update 7.3-TestForCyclicity.py

--- Python/7-LinkedLists/7.3-TestForCyclicity.py
+++ Python/7-LinkedLists/7.3-TestForCyclicity.py
@@ -1,1 +1,16 @@
+# LC Link: https://leetcode.com/problems/linked-list-cycle/
 
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+		slow = fast = head
+		
+		while slow and fast and fast.next:
+			fast = fast.next
+			if slow == fast:
+				return True
+			if fast.next:
+				fast = fast.next
+			slow = slow.next
+		
+		return False
+    	
